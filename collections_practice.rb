85060659d0779ca5547b667f3eb836ce1a933c62
Dan Hintz <dhintz@live.com>
2018-08-27 23:54:54 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -21,3 +21,15 @@ def sort_array_desc(array)
     end
   end
 end
+
+def sort_array_asc(array)
+  array.sort do |a,b|
+    if a < b
+      -1
+    elsif a == b
+      0
+    elsif a > b
+      1
+    end
+  end
+end
